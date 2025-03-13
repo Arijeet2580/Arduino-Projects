d291f22e154a3cc02e3d999d939af590f05e2e5d
Arijeet Kar <kararijeet26@gmail.com>
2025-03-13 12:21:39 +0530

    Update BluetoothComms_DigitalDice.ino

--- P5_Digital_Dice_Bluetooth/BluetoothComms_DigitalDice/BluetoothComms_DigitalDice.ino
+++ P5_Digital_Dice_Bluetooth/BluetoothComms_DigitalDice/BluetoothComms_DigitalDice.ino
@@ -1,30 +1,36 @@
 #include <SoftwareSerial.h>
+
 const byte rxPin = 9;
 const byte txPin = 10;
-SoftwareSerial Bluetooth(rxPin,txPin);
+SoftwareSerial Bluetooth(rxPin, txPin);
+
+String msgBuffer = "";
+String msg = "";
+char data = '';
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(rxPin,INPUT);
-  pinMode(txPin,OUTPUT);
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
   Bluetooth.begin(9600);
   Serial.begin(9600);
 }
-String msgBuffer = " ";
-String msg ="";
-char data="";
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-  while(Bluetooth.available() == 0){}
-  char Data = (char) Bluetooth.read(); // Break the incoming data into bits
-  if(data == ';'){
-    msg=msgBuffer;
-    msgBuffer=" ";//Clears the message Buffer for new Data
-    Serial.println(msg);
-    Bluetooth.print("Your chance ");
-    Bluetooth.print(msg);
-    int num = random(1,7);
-    Bluetooth.println(num);
+  if (Bluetooth.available() > 0) {
+    char Data = (char)Bluetooth.read(); // Read the incoming data
+
+    if (Data == ';') { // End of message delimiter
+      msg = msgBuffer;
+      msgBuffer = ""; // Clear buffer for new data
+      Serial.println("Received: " + msg);
+
+      // Send response back to Bluetooth
+      Bluetooth.print("Your chance: ");
+      Bluetooth.print(msg);
+      int num = random(1, 7);
+      Bluetooth.println(" " + String(num));
+    } else {
+      msgBuffer += Data; // Append data to buffer
+    }
   }
-  msgBuffer +=Data;
 }
